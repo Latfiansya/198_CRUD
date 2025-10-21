1df375c7defabca76866c8b5b9af84129bba2ed8
Latfiansya Dzikri - 198 <latfiansyadzikri@gmail.com>
2025-10-21 14:20:21 +0700

    menguji server

--- index.js
+++ index.js
@@ -3,5 +3,16 @@ const express = require('express');
 let mysql = require('mysql');
 const app = express();
 const port = 3000;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//menguji server
+app.get('/', (req, res) => {
+    res.send('Halo Dunia!');
+});
+
+app.listen(port, () => {
+    console.log(`Server berjalan di port ${port}`);
+});
+
