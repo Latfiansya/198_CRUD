db396624f17e1d08a2ea88d27a5252faf32aba86
Latfiansya Dzikri - 198 <latfiansyadzikri@gmail.com>
2025-10-21 14:17:13 +0700

    Setup server menggunakan Express.js dan MySQL

---
+++ index.js
@@ -0,0 +1,7 @@
+//Setup server menggunakan Express.js dan MySQL
+const express = require('express');
+let mysql = require('mysql');
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
